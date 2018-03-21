caeee37e55ef4865ea16e5cc63e8687a763502cf
vincent BOCHET <vbochet@excilys.com>
2018-03-21 11:15:25 +0100

    [Feat] Add ComputerDaoPage for use in webpages

---
+++ src/main/java/com/excilys/formation/cdb/paginator/ComputerDtoPage.java
@@ -0,0 +1,22 @@
+package com.excilys.formation.cdb.paginator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import com.excilys.formation.cdb.dto.ComputerDto;
+import com.excilys.formation.cdb.mapper.ComputerMapper;
+import com.excilys.formation.cdb.model.Computer;
+import com.excilys.formation.cdb.service.ComputerService;
+
+public class ComputerDtoPage extends Page<ComputerDto> {
+
+    protected void refreshContent() {
+        List<Computer> computerList = ComputerService.INSTANCE.getList(getOffset(), getNbPerPage());
+        List<ComputerDto> computerDtoList = new ArrayList<>();
+        Consumer<Computer> computerConsumer = (x) -> computerDtoList.add(ComputerMapper.INSTANCE.computerToComputerDto(x));
+        computerList.forEach(computerConsumer);
+        setContent(computerDtoList);
+    }
+
+}
