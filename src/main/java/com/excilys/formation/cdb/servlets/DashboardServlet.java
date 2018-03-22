135fdbc68b0566a261309925ef1527ff4b1dd3a6
vincent BOCHET <vbochet@excilys.com>
2018-03-22 10:32:30 +0100

    [Refacto] Use annotation instead of web.xml to declare servlet bindings

--- src/main/java/com/excilys/formation/cdb/servlets/DashboardServlet.java
+++ src/main/java/com/excilys/formation/cdb/servlets/DashboardServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,6 +16,7 @@ import org.slf4j.LoggerFactory;
 import com.excilys.formation.cdb.paginator.ComputerDtoPage;
 import com.excilys.formation.cdb.service.ComputerService;
 
+@WebServlet("/dashboard")
 public class DashboardServlet extends HttpServlet {
 
     static final Logger LOGGER = LoggerFactory.getLogger(DashboardServlet.class);
