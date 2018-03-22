a42a12f28967927b98438c0c96315c0f4336c597
vincent BOCHET <vbochet@excilys.com>
2018-03-22 15:41:40 +0100

    [Test] Add tests for DTO mappers

--- src/test/java/com/excilys/formation/cdb/mapper/CompanyMapperTest.java
+++ src/test/java/com/excilys/formation/cdb/mapper/CompanyMapperTest.java
@@ -8,12 +8,13 @@ import java.sql.SQLException;
 
 import org.junit.Test;
 
+import com.excilys.formation.cdb.dto.CompanyDto;
 import com.excilys.formation.cdb.model.Company;
 
 public class CompanyMapperTest {
 
 	@Test
-	public void createCompanyTest() throws SQLException {
+	public void resultSetToCompanyTest() throws SQLException {
 		//given
 		final int ID = 42;
 		final String NAME = "toto";
@@ -29,5 +30,65 @@ public class CompanyMapperTest {
 		assertEquals(ID, company.getId());
 		assertEquals(NAME, company.getName());
 	}
+
+
+    @Test
+    public void companyToCompanyDtoTest() throws SQLException {
+        //given
+        final int ID = 42;
+        final int NO_ID = 0;
+        final String NAME = "toto";
+        Company company;
+        CompanyDto companyDto;
+
+        //when
+        company = new Company(ID, NAME);
+        companyDto = CompanyMapper.INSTANCE.companyToCompanyDto(company);
+
+        //then
+        assertEquals(ID, companyDto.getCompanyId());
+        assertEquals(NAME, companyDto.getCompanyName());
+
+
+        //when
+        company = new Company(NO_ID, null);
+        companyDto = CompanyMapper.INSTANCE.companyToCompanyDto(company);
+
+        //then
+        assertEquals(NO_ID, companyDto.getCompanyId());
+        assertEquals(null, companyDto.getCompanyName());
+    }
+
+
+    @Test
+    public void companyDtoToCompanyTest() throws SQLException {
+        //given
+        final int ID = 42;
+        final int NO_ID = 0;
+        final String NAME = "toto";
+        Company company;
+        CompanyDto companyDto;
+
+        //when
+        companyDto = new CompanyDto();
+        companyDto.setCompanyId(ID);
+        companyDto.setCompanyName(NAME);
+        company = CompanyMapper.INSTANCE.companyDtoToCompany(companyDto);
+
+        //then
+        assertEquals(ID, company.getId());
+        assertEquals(NAME, company.getName());
+
+
+        //when
+        companyDto = new CompanyDto();
+        companyDto.setCompanyId(NO_ID);
+        companyDto.setCompanyName(null);
+        company = CompanyMapper.INSTANCE.companyDtoToCompany(companyDto);
+
+        //then
+        assertEquals(NO_ID, company.getId());
+        assertEquals(null, company.getName());
+    }
 
 }
