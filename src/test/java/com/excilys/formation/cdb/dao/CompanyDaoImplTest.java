945a9e27a7cc829a8340fef526317d7c84be95a2
vincent BOCHET <vbochet@excilys.com>
2018-04-09 11:13:59 +0200

    [Fix] Replace 'computer' with 'company' in variable names

--- src/test/java/com/excilys/formation/cdb/dao/CompanyDaoImplTest.java
+++ src/test/java/com/excilys/formation/cdb/dao/CompanyDaoImplTest.java
@@ -10,9 +10,12 @@ import java.sql.Statement;
 
 import org.junit.*;
 
+import com.excilys.formation.cdb.exceptions.DaoException;
+import com.excilys.formation.cdb.model.Company;
+
 public class CompanyDaoImplTest {
     
-    final static int NB_COMPUTERS_IN_DB = 25;
+    final static int NB_COMPANY_IN_DB = 25;
 
     @BeforeClass
     public static void init() throws SQLException, ClassNotFoundException, IOException {
@@ -33,8 +36,8 @@ public class CompanyDaoImplTest {
         try (Connection connection = getConnection(); Statement statement = connection.createStatement();) {
             statement.execute("CREATE TABLE company (id bigint not null, name varchar(255), constraint pk_company primary key (id));");
             connection.commit();
-            for(int i = 0; i < NB_COMPUTERS_IN_DB; i++) {
-                statement.executeUpdate("insert into company (id,name) values (  "+i+",'computer "+i+"');");
+            for(int i = 0; i < NB_COMPANY_IN_DB; i++) {
+                statement.executeUpdate("insert into company (id,name) values (  "+i+",'company "+i+"');");
             }
             connection.commit();
         }
@@ -45,10 +48,9 @@ public class CompanyDaoImplTest {
     }
 
     @Test
-    public void listTest() { //throws DaoException {
+    public void listTest() throws DaoException {
         assertEquals(10, CompanyDaoImpl.INSTANCE.list(0, 10).size());
-        assertEquals(NB_COMPUTERS_IN_DB, CompanyDaoImpl.INSTANCE.list(0, NB_COMPUTERS_IN_DB).size());
-        assertEquals(NB_COMPUTERS_IN_DB, CompanyDaoImpl.INSTANCE.list(0, 0).size());
+        assertEquals(NB_COMPANY_IN_DB, CompanyDaoImpl.INSTANCE.list(0, NB_COMPANY_IN_DB).size());
+        assertEquals(NB_COMPANY_IN_DB, CompanyDaoImpl.INSTANCE.list(0, 0).size());
     }
-
 }
