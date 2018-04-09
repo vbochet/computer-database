dd247914b50262ec3dc7590eab2577389c1cdc02
vincent BOCHET <vbochet@excilys.com>
2018-04-09 12:31:16 +0200

    [Test] Add test case for remaining methods in ComputerDao

--- src/test/java/com/excilys/formation/cdb/dao/ComputerDaoImplTest.java
+++ src/test/java/com/excilys/formation/cdb/dao/ComputerDaoImplTest.java
@@ -7,15 +7,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDate;
+import java.util.Optional;
 
 import org.junit.*;
 
 import com.excilys.formation.cdb.exceptions.DaoException;
+import com.excilys.formation.cdb.model.Company;
+import com.excilys.formation.cdb.model.Computer;
 
 public class ComputerDaoImplTest {
     
-    final static int NB_COMPUTER_IN_DB = 25;
+    static int NB_COMPUTER_IN_DB = 25;
 
     @BeforeClass
     public static void init() throws SQLException, ClassNotFoundException, IOException {
@@ -55,6 +57,68 @@ public class ComputerDaoImplTest {
         assertEquals(10, ComputerDaoImpl.INSTANCE.list(0, 10).size());
         assertEquals(NB_COMPUTER_IN_DB, ComputerDaoImpl.INSTANCE.list(0, NB_COMPUTER_IN_DB).size());
         assertEquals(NB_COMPUTER_IN_DB, ComputerDaoImpl.INSTANCE.list(0, 0).size());
+    }
+
+    @Test
+    public void readTest() {
+        Computer computerExpected = null;
+        Computer computerRead = null;
+        for(int i = 0; i < NB_COMPUTER_IN_DB; i++) {
+            computerExpected = new Computer(i, "computer "+i, null, null, new Company(i, "company "+i));
+            computerRead = ComputerDaoImpl.INSTANCE.read(i).get();
+
+            assertEquals(computerExpected.getId(), computerRead.getId());
+            assertEquals(computerExpected.getName(), computerRead.getName());
+            assertEquals(computerExpected.getIntroduced(), computerRead.getIntroduced());
+            assertEquals(computerExpected.getDiscontinued(), computerRead.getDiscontinued());
+            assertEquals(computerExpected.getCompany().getId(), computerRead.getCompany().getId());
+            assertEquals(computerExpected.getCompany().getName(), computerRead.getCompany().getName());
+        }
+    }
+
+    @Test
+    public void createTest() {
+        Computer computer = new Computer(42, "computer 42", null, null, new Company(1, "company 1"));
+        Computer computerCreated = ComputerDaoImpl.INSTANCE.create(computer);
+
+        assertEquals(computer.getId(), computerCreated.getId());
+        assertEquals(computer.getName(), computerCreated.getName());
+        assertEquals(computer.getIntroduced(), computerCreated.getIntroduced());
+        assertEquals(computer.getDiscontinued(), computerCreated.getDiscontinued());
+        assertEquals(computer.getCompany().getId(), computerCreated.getCompany().getId());
+        assertEquals(computer.getCompany().getName(), computerCreated.getCompany().getName());
+        
+        NB_COMPUTER_IN_DB++;
+    }
+
+    @Test
+    public void updateTest() {
+        Computer computer = new Computer(1, "computer 100", null, null, new Company(10, "company 10"));
+        Computer computerUpdated = ComputerDaoImpl.INSTANCE.update(computer);
+
+        assertEquals(computer.getId(), computerUpdated.getId());
+        assertEquals(computer.getName(), computerUpdated.getName());
+        assertEquals(computer.getIntroduced(), computerUpdated.getIntroduced());
+        assertEquals(computer.getDiscontinued(), computerUpdated.getDiscontinued());
+        assertEquals(computer.getCompany().getId(), computerUpdated.getCompany().getId());
+        assertEquals(computer.getCompany().getName(), computerUpdated.getCompany().getName());
+
+        computer = new Computer(1, "computer 1", null, null, new Company(1, "company 1"));
+        ComputerDaoImpl.INSTANCE.update(computer);
+    }
+
+    @Test
+    public void deleteTest() {
+        ComputerDaoImpl.INSTANCE.delete(5);
+        NB_COMPUTER_IN_DB++;
+        
+        Optional<Computer> oc = ComputerDaoImpl.INSTANCE.read(5);
+        assertEquals(Optional.empty(), oc);
+    }
+
+    @Test
+    public void countTest() {
+        assertEquals(NB_COMPUTER_IN_DB, ComputerDaoImpl.INSTANCE.count());
     }
 
 }
