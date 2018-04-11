4cabb98092e8931819df0e2285d8abcff5a5cadb
vincent BOCHET <vbochet@excilys.com>
2018-04-11 11:46:24 +0200

    [Refacto] Remove unused imports

--- src/main/java/com/excilys/formation/cdb/paginator/ComputerDtoPage.java
+++ src/main/java/com/excilys/formation/cdb/paginator/ComputerDtoPage.java
@@ -6,8 +6,6 @@ import java.util.function.Consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import com.excilys.formation.cdb.dto.ComputerDto;
 import com.excilys.formation.cdb.exceptions.PageException;
