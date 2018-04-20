0431c9fe35dae4f23de90d892ac4a1778fc2fabc
vincent BOCHET <vbochet@excilys.com>
2018-04-20 12:01:36 +0200

    [Refacto] Remove unused imports

--- src/main/java/com/excilys/formation/cdb/dao/ComputerDaoImpl.java
+++ src/main/java/com/excilys/formation/cdb/dao/ComputerDaoImpl.java
@@ -14,7 +14,6 @@ import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
