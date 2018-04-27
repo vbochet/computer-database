03320e8654d849d542a3881751b62542546b1370
vincent BOCHET <vbochet@excilys.com>
2018-04-27 11:39:55 +0200

    [Refacto] Remove unused imports in ComputerDaoImpl

--- computer-database-persistence/src/main/java/com/excilys/formation/cdb/dao/ComputerDaoImpl.java
+++ computer-database-persistence/src/main/java/com/excilys/formation/cdb/dao/ComputerDaoImpl.java
@@ -12,12 +12,9 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Order;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.excilys.formation.cdb.model.Company_;
