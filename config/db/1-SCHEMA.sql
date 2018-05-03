bc221f811a99cce510751305e5d8440610fb7d17
vincent BOCHET <vbochet@excilys.com>
2018-05-03 10:44:12 +0200

    [UPD] Update sql files for tables used in SpringSecurity

--- config/db/1-SCHEMA.sql
+++ config/db/1-SCHEMA.sql
@@ -22,20 +22,12 @@ drop schema if exists `computer-database-db`;
 
   create table users (
     username                  varchar(50) not null,
-    password                  varchar(50) not null,
+    password                  varchar(100) not null,
     enabled                   tinyint not null default 1,
+    role                      varchar(50) not null,
     constraint pk_users primary key (username))
-  ;
-
-  create table user_roles (
-    user_role_id              int(11) NOT null auto_increment,
-    username                  varchar(50) NOT null,
-    role                      varchar(50) NOT null,
-    constraint pk_user_roles primary key (user_role_id),
-    UNIQUE KEY uni_username_role (role,username))
   ;
 
 
   alter table computer add constraint fk_computer_company_1 foreign key (company_id) references company (id) on delete restrict on update restrict;
-  alter table user_roles add constraint fk_username_1 foreign key (username) references users (username) on delete restrict on update restrict;
-  create index ix_computer_company_1 on computer (company_id);
+
